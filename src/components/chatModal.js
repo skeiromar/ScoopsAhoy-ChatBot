4c84e338925fe74cb3e5c185831c9a87d71137ac
skeiron <omar.work2@gmail.com>
2019-08-01 04:12:37 -0400

    made size changes to the modal

--- src/components/chatModal.js
+++ src/components/chatModal.js
@@ -34,7 +34,10 @@ const modalStyles = {
       right                 : 'auto',
       bottom                : 'auto',
       marginRight           : '-50%',
-      transform             : 'translate(-50%, -50%)'
+      transform             : 'translate(-50%, -50%)',
+      maxWidth: '50%',
+      justifyContent: 'center',
+      display: 'flex'
     }
   };
 
